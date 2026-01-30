3fc884676818331d563058dd233223ab10ec0b24
safronman <safronmanbox@gmail.com>
2026-01-30 19:40:13 +0300

    fix: Bestsellers finish

--- src/BestSellers.tsx
+++ src/BestSellers.tsx
@@ -19,7 +19,7 @@ export const BestSellers = () => {
     },
     {
       _id: '664476983793ba7564c50ba2',
-      id: 8,
+      id: 9,
       title: 'Zip hoodie 23',
       price: 291.99,
       description: 'Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.',
@@ -32,26 +32,39 @@ export const BestSellers = () => {
       createdAt: '2024-05-15T08:47:20.180Z',
       updatedAt: '2024-05-15T12:20:34.117Z',
       __v: 0
-    }
+    },
+    {
+      _id: '664476983793ba7564c50ba2',
+      id: 10,
+      title: 'Zip hoodie 234',
+      price: 4291.99,
+      description: 'Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.',
+      category: 'men\'s clothing',
+      image: 'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png',
+      rating: {
+        rate: 4.7,
+        count: 120
+      },
+      createdAt: '2024-05-15T08:47:20.180Z',
+      updatedAt: '2024-05-15T12:20:34.117Z',
+      __v: 0
+    },
   ];
-
 
   return (
     <div className={'bestSeller'}>
       <h2>Bestsellers</h2>
       <div className={'cards'}>
-        <div className="card">
-          <img src={products[0].image} alt="img"/>
-          <h4>{products[0].title}</h4>
-          <p className="price">${products[0].price}</p>
-          <button>Show more</button>
-        </div>
-        <div className="card">
-          <img src={products[1].image} alt="img"/>
-          <h4>{products[1].title}</h4>
-          <p className="price">${products[1].price}</p>
-          <button>Show more</button>
-        </div>
+        {products.map((el) => {
+          return (
+            <div className="card" key={el.id}>
+              <img src={el.image} alt="img"/>
+              <h4>{el.title}</h4>
+              <p className="price">${el.price}</p>
+              <button>Show more</button>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
