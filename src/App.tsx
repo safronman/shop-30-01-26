98eabf8554a6aad3fd5bef411ffe46056b7242b7
safronman <safronmanbox@gmail.com>
2026-01-30 20:21:21 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
 import {Header} from './Header.tsx';
 import {BestSellers} from './BestSellers.tsx';
+import {Route, Routes} from 'react-router';
+import {Product} from './Product.tsx';
 
 function App() {
 
   return (
     <div className={'appContainer'}>
       <Header/>
-      <BestSellers/>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<BestSellers/>}/>
+        <Route path="/produc" element={<Product/>}/>
+      </Routes>
     </div>
   )
 }
